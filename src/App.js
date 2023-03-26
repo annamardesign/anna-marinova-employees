63737b2b6aa3a42ea36537c6cca7f6fe738aab49
Anna Marinova <lalogica@gmail.com>
2023-03-26 21:34:34 +0300

    Added logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { csvToJson, getHeaders } from "./common";
+import { csvToJson, getHeaders, getDays, groupByProjectId, hasOverlap, calculateOverlap, findAllPairsIndexes } from "./common";
 import moment from 'moment';
 import './App.css';
  
  function App() {
    const [file, setFile] = useState();
-   const [json, setJson] = useState([]);
+   const [parsedRecords, setParsedRecords] = useState([]);
    const [headers, setHeaders] = useState([]);
  
    const fileReader = new FileReader();
@@ -17,17 +17,75 @@ import './App.css';
    moment.defaultFormat = "YYYY-MM-DD";
    const csvFileToArray = string => {
     setHeaders(getHeaders(string));
-    setJson(csvToJson(string))
+    setParsedRecords(csvToJson(string))
   }
-  console.log('json', json);
-  if(json) {
-  json.forEach(j => {
-    console.log('j', j.DateFrom);
-   j.DateFrom = moment(j.DateFrom, 'YYYY-MM-DD').toDate()
-   j.DateTo = moment(j.DateTo, 'YYYY-MM-DD').toDate()
-  })
+  console.log('json', parsedRecords);
+  
+
+
+  const newTable = [];
+  let uniqueProjectIds = [];
+  let filteredPairs = [];
+
+  if(parsedRecords) {
+    parsedRecords.forEach(item => {
+      item.DateFrom = moment(item.DateFrom, 'YYYY-MM-DD').toDate();
+      if(item.DateTo == 'Invalid Date') {
+        item.DateTo = new Date();
+      }
+      item.DateTo = moment(item.DateTo, 'YYYY-MM-DD').toDate();
+      item.Days = getDays(item.DateTo, item.DateFrom);
+      const itemNoDates = JSON.stringify(item, (key, value) => {
+        if (key == "DateFrom" || key == "DateTo") {
+          return undefined;
+        }
+        return value;
+      });
+      newTable.push(JSON.parse(itemNoDates));
+    })
+
+    uniqueProjectIds = [...new Set(parsedRecords.map(item => item.ProjectId))];
+    uniqueProjectIds.forEach(projectId => {
+      const projectRecords = parsedRecords.filter(r => r.ProjectId === projectId);
+      if (projectRecords.length > 1) {
+        if (projectRecords.length === 2){
+          if (hasOverlap(projectRecords[0], projectRecords[1])) {
+            const overlapDuration = calculateOverlap(projectRecords[0], projectRecords[1]);
+            filteredPairs.push({EmpID1: projectRecords[0].EmpId, EmpID2: projectRecords[1].EmpId, ProjectId: projectId, Days: overlapDuration})
+          }
+        }
+        if  (projectRecords.length > 2){
+          const allPairsIndexes = findAllPairsIndexes(projectRecords.length)
+          allPairsIndexes.forEach(pair => {
+            if (hasOverlap(projectRecords[pair.index1], projectRecords[pair.index2])){
+              const overlapDuration = calculateOverlap(projectRecords[pair.index1], projectRecords[pair.index2]);
+              filteredPairs.push({EmpID1: projectRecords[pair.index1].EmpId, EmpID2: projectRecords[pair.index2].EmpId, ProjectId: projectId, Days: overlapDuration})
+            }
+          })
+        }
+      }
+    })
+
+    console.log('filteredPairs', filteredPairs);
+    const sortedPairs = filteredPairs.sort((a,b) => b.Days - a.Days)
+    console.log('sortedPairs', sortedPairs);
+
+    console.log('winners', sortedPairs[0])
+
+
+
+
+    const test = parsedRecords.filter(r => r.ProjectId === '14');
+    if(test.length > 0) {
+      console.log('test', test);
+      const overlap = hasOverlap(test[0], test[1]);
+      const calculate = calculateOverlap(test[0], test[1]);
+      console.log('overlap', overlap);
+    }
   };
-  console.log('json2', json);
+
+  const grouped = groupByProjectId(newTable);
+    console.log('grouped', grouped);
  
    const handleOnSubmit = (e) => {
      e.preventDefault();
@@ -65,7 +123,7 @@ import './App.css';
  
        <br />
  
-       <table>
+       {/* <table>
          <thead>
            <tr key={"header"}>
              {headers && headers.map((key) => (
@@ -75,7 +133,7 @@ import './App.css';
          </thead>
  
           <tbody>
-           {json.map((item) => (
+           {newTable && newTable.map((item) => (
              <tr key={item.id}>
                {Object.values(item).map((val) => (
                  <td>{val.toString()}</td>
@@ -83,7 +141,7 @@ import './App.css';
              </tr>
            ))}
          </tbody>
-       </table>
+       </table> */}
      </div>
    );
  }
--- src/common.js
+++ src/common.js
@@ -1,4 +1,17 @@
 const delimeter = ',';
+const getLaterDate = (firstDate, secondDate) => {
+        if(firstDate < secondDate) {
+                return secondDate
+        }
+        return firstDate;
+};
+const getSoonerDate = (firstDate, secondDate) => {
+        if(firstDate < secondDate) {
+                return firstDate
+        }
+        return secondDate;
+};
+
 export const csvToJson = (csv) => {
         const lines = csv.split('\r\n');
       
@@ -18,8 +31,56 @@ export const csvToJson = (csv) => {
           result.push(obj);
         }
         return result;
-      }
+}
 export const getHeaders = (csv) => {
         const lines = csv.split('\r\n');
         return lines.shift().split(delimeter);   
 }
+export const getDays = (date_1, date_2) =>{
+        let difference = date_1.getTime() - date_2.getTime();
+        let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+        return TotalDays;
+};
+export const getDaysFromUnix = (timestamp1, timestamp2) => {
+        let difference = timestamp1 - timestamp2;
+        let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+        return TotalDays;
+};
+export  const groupByProjectId = (obj) => {
+        return Object.values(obj.reduce((acc, current) => {
+            acc[current.projectId] = acc[current.projectId] ?? [];
+            acc && acc[current.projectId].push(current);
+            return acc;
+        }, {})
+    );
+};
+
+export const hasOverlap = (firstRecord, secondRecord) => {
+        return (firstRecord.DateFrom.getTime() <= secondRecord.DateTo.getTime()
+                && (firstRecord.DateTo.getTime() >= secondRecord.DateTo.getTime()))
+};
+
+export const calculateOverlap = (firstRecord, secondRecord) => {
+        const periodStart = getLaterDate(firstRecord.DateFrom.getTime(), secondRecord.DateFrom.getTime());
+        const periodEnd = getSoonerDate(secondRecord.DateTo.getTime(), firstRecord.DateTo.getTime());
+        return getDaysFromUnix(periodEnd, periodStart);
+};
+
+export const findAllPairsIndexes = (arrLength) => {
+        let arrIndexes = [];
+
+        for (let i = 0; i < arrLength; i++){
+                arrIndexes.push(i);
+        }
+
+        var i, j, result = [];
+        for (i = 0; i < arrIndexes.length; i++) {
+            for (j = i; j < arrIndexes.length; j++) {
+                if (i !== j) {
+                        result.push({ index1: arrIndexes[i], index2: arrIndexes[j]})
+                }
+            }
+        }
+        console.log("*****************" , result)
+        return result;
+};
